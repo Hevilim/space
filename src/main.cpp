af98ef6abf7826b5490d67154cb2039cc4b9828e
Hevilim <karimaliyev1@icloud.com>
2025-05-05 12:36:40 +0500

    DnD fill .bmp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <cstdio>
 
 int main(int argc, char *argv[])
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Surface *surface;
+
+    // convert -size 100x100 xc:gray sample.bmp
     SDL_Texture *texture;
+    
     SDL_Event event;
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -19,30 +23,51 @@ int main(int argc, char *argv[])
         return 3;
     }
 
-    surface = SDL_LoadBMP("sample.bmp");
-    if (!surface) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
-        return 3;
-    }
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
-        return 3;
-    }
-    SDL_DestroySurface(surface);
+    // surface = SDL_LoadBMP(path);
+    // if (!surface) {
+    //     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
+    //     return 3;
+    // }
+    // texture = SDL_CreateTextureFromSurface(renderer, surface);
+    // if (!texture) {
+    //     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
+    //     return 3;
+    // }
+    // SDL_DestroySurface(surface);
 
     while (1) {
         SDL_PollEvent(&event);
         if (event.type == SDL_EVENT_QUIT) {
             break;
+        } else if (event.type == SDL_EVENT_DROP_FILE) {
+            const char* path = (const char*)event.drop.data;
+            SDL_Log("Dropped file: %s", path);
+
+            SDL_Surface* surface = SDL_LoadBMP(path);
+            if (!surface) {
+                SDL_Log("Failed to load BMP: %s", SDL_GetError());
+                continue;
+            }
+
+            if (texture) SDL_DestroyTexture(texture);
+            texture = SDL_CreateTextureFromSurface(renderer, surface);
+            SDL_DestroySurface(surface);
+
+            if (!texture) {
+                SDL_Log("Failed to create texture: %s", SDL_GetError());
+            }
         }
+
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(renderer);
-        SDL_RenderTexture(renderer, texture, NULL, NULL);
+        // SDL_RenderTexture(renderer, texture, NULL, NULL);
+        if (texture) {
+            SDL_RenderTexture(renderer, texture, NULL, NULL);
+        }
         SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyTexture(texture);
+    if (texture) SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
